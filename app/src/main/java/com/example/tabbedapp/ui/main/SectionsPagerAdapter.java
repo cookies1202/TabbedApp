872d5308e6e6d865920e8e712e8ef31815491c9c
Haas Manuel <manuel.haas@edu.fh-joanneum.at>
2025-03-19 13:28:22 +0100

    Initial commit

---
+++ app/src/main/java/com/example/tabbedapp/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,64 @@
+package com.example.tabbedapp.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.tabbedapp.R;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2, R.string.tab_text_3};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return PlaceholderFragment.newInstance(1); // für "Patient"
+            case 1:
+                return PlaceholderFragment.newInstance(2); // für "Behandlung"
+            case 2:
+                return PlaceholderFragment.newInstance(3); // für "Rechnung"
+            default:
+                return PlaceholderFragment.newInstance(-1); // Fallback
+        }
+    }
+
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Patient";
+            case 1:
+                return "Behanldung";
+            case 2:
+                return "Rechnung";
+            default:
+                return "unbekannt";
+
+
+        }
+    }
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        return 3;
+    }
+}
